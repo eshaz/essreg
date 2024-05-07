8009c657af7d694d32940b93fdf84bdc0164f71e
Ethan Halsall <ethanhalsall11@augustana.edu>
2024-05-06 20:00:39 -0500

    feat: add build scripts

--- source.bat
+++ source.bat
@@ -1,10 +1,4 @@
-del *.obj
 PATH C:\WINDOWS;C:\WINDOWS\COMMAND;C:\NETCOM;%PATH%;C:\WATCOM\BINNT;C:\WATCOM\BINW
 SET INCLUDE=C:\WATCOM\H;C:\WATCOM\MFC\INCLUDE;C:\WATCOM\H\NT
 SET WATCOM=C:\WATCOM
 SET EDPATH=C:\WATCOM\EDDAT
-wmake
-copy essreg.exe build\essreg.exe /y
-copy 1869opl3.com build\1869opl3.com /y
-copy 1869opl3.com build\esfmpat.exe /y
-
